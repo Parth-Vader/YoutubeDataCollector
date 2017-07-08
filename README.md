aa077b0997b10127af5238c490ae34aadc42779d
Parth-vader <vermaparth97@gmail.com>
2017-07-08 17:19:23 +0530

    extension changed

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# YoutubeDataCollector
+# My Youtube History
+
+Searches for recent youtube videos opened and displays them in a list form.
+
+### Chrome
+
+- Visit `chrome://extensions`
+- Enable Developer Mode.
+- Click on the "Load unpacked extension..."
+- Select the directory in which this repo is cloned.
---
+++ ext/history.js
@@ -0,0 +1,26 @@
+function showAll(e) {
+  var root = document.getElementById("history");
+  root.innerHTML = "";
+  chrome.history.search(
+    {text: "www.youtube.com/watch?v=", maxResults: 20},
+    function(results) {
+      if (results.length < 1) {
+        var li = document.createElement('li');
+        var tn = document.createTextNode('Empty.');
+        li.appendChild(tn);
+        root.appendChild(li);
+      } else {
+        for (var k in results) {
+          var li = document.createElement('li');
+          var history = results[k];
+          var tn = document.createTextNode("URL: " + history.url + ", Title: " + history.title);
+          li.appendChild(tn);
+          root.appendChild(li);
+        }
+      }
+    }
+  );
+}
+
+document.getElementById('update').addEventListener('click', showAll);
+
