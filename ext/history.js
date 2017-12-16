dad8ea43b22ab77130c2ca91d20d289944af2166
Aniket Gattani <aniketgattani@gmail.com>
2017-12-16 12:25:13 +0530

    Add bookmark option added Readme updated (#11)

--- ext/history.js
+++ ext/history.js
@@ -13,13 +13,32 @@ function showAll(e) {
         for (var k in results) {
           var li = document.createElement('li');
           var history = results[k];
-          var tn = document.createTextNode("URL: " + history.url + ", Title: " + history.title);
+          var tn = document.createTextNode("URL: " + history.url + ", Title: " + history.title+' ');
+          var a = document.createElement('a');
+          a.setAttribute('href',"#");
+          a.onclick=function(evt){
+            var text= evt.target.parentElement.innerText;
+            var url= text.split(' ')[1];
+            var title= text.substring(text.indexOf('Title:')+7,text.indexOf(evt.target.innerText)+1);
+            addBookmark(url,title);
+          };
+          a.innerHTML = 'Add Bookmark';
           li.appendChild(tn);
+          li.appendChild(a);
           root.appendChild(li);
         }
       }
     }
   );
+}
+
+function addBookmark(url,title){
+  chrome.bookmarks.search(url,function(result){
+    if (result.length==0){
+      chrome.bookmarks.create({'parentId': '1','title': title,'url':url});
+    }
+  })
+   
 }
 
 document.getElementById('update').addEventListener('click', showAll);
