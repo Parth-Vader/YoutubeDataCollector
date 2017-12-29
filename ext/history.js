d652bbefe752d45dda7db228b3ba0c2c3a1df72f
Yash <yash@email.com>
2017-12-29 14:09:14 +0530

    Corrected the Error Regarding Bookmark and Opening the video

--- ext/history.js
+++ ext/history.js
@@ -15,14 +15,25 @@ function showAll(e) {
           var history = results[k];
           var tn = document.createTextNode("URL: " + history.url + ", Title: " + history.title+' ');
           var a = document.createElement('a');
+          var b = document.createElement('b');
           a.setAttribute('href',"#");
+          b.setAttribute('href',"#");
+          b.onclick=function(evt1){
+          	var text= evt1.target.parentElement.innerText;
+          	var url = text.split(' ')[4];
+          	alert(url);
+          	window.open(url);
+          };
           a.onclick=function(evt){
             var text= evt.target.parentElement.innerText;
-            var url= text.split(' ')[1];
+            var url= text.split(' ')[4];
+            alert(url);
             var title= text.substring(text.indexOf('Title:')+7,text.indexOf(evt.target.innerText)+1);
             addBookmark(url,title);
           };
+          b.innerHTML = 'Click to Open ';
           a.innerHTML = 'Add Bookmark';
+          li.appendChild(b);
           li.appendChild(tn);
           li.appendChild(a);
           root.appendChild(li);
